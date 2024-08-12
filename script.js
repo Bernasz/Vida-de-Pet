5bcd0242f180376dbee2516daea927ab13563771
joaoConrad <joaovitorconrad@gmail.com>
2024-08-12 01:13:55 -0300

    Adição do Input para Especificar o Tipo de Pet

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 //Adiciona textbox ao selecionar a opção "Outro"
+
+//Variaveis para selecionar os elementos do HTML
 const opcaoPet = document.getElementById("opcaoPet")
 const containerInput = document.getElementById("containerInput")
 
-opcaoPet.addEventListener('change', function caixaTexto(){
+opcaoPet.addEventListener('change', function caixaTexto(){ //Função é executada quando o item do Select é alterado
 
     containerInput.innerHTML = ''
 
-    if (this.value === "Outro")
+    if (this.value === "Outro") //Se o valor selecionado for "Outro", o bloco abaixo é executado
         {
+            //Cria o Input de Texto
            const inputTxt = document.createElement("input")
            inputTxt.type = "text"
            inputTxt.placeholder = "Especifique"
            inputTxt.id = 'inputOutra'
 
-           containerInput.appendChild(inputTxt)
+           containerInput.appendChild(inputTxt) //Exibe o Input na Tela do Usuário
         }
 
 }
