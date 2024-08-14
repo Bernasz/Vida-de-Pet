f70d25e4237ffe363a85339a0f55e6e25fb790ac
joaoConrad <joaovitorconrad@gmail.com>
2024-08-14 19:36:46 -0300

    Correção Bug (Especificar Tipo Pet)

--- script.js
+++ script.js
@@ -4,20 +4,20 @@
 const opcaoPet = document.getElementById("opcaoPet")
 const containerInput = document.getElementById("containerInput")
 
-opcaoPet.addEventListener('change', function caixaTexto(){ //Função é executada quando o item do Select é alterado
+opcaoPet.addEventListener('change', function caixaTexto() { //Função é executada quando o item do Select é alterado
 
     containerInput.innerHTML = ''
 
     if (this.value === "Outro") //Se o valor selecionado for "Outro", o bloco abaixo é executado
-        {
-            //Cria o Input de Texto
-           const inputTxt = document.createElement("input")
-           inputTxt.type = "text"
-           inputTxt.placeholder = "Especifique"
-           inputTxt.id = 'inputOutra'
+    {
+        //Cria o Input de Texto
+        const inputTxt = document.createElement("input")
+        inputTxt.type = "text"
+        inputTxt.placeholder = "Especifique"
+        inputTxt.id = 'inputOutra'
 
-           containerInput.appendChild(inputTxt) //Exibe o Input na Tela do Usuário
-        }
+        containerInput.appendChild(inputTxt) //Exibe o Input na Tela do Usuário
+    }
 
 }
 )
